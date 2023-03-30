d1b72f5397b4f3aa9ef30c4f07ea120fd6150cfe
Madar iOS <madar.developer.iosapps@gmail.com>
2023-03-31 00:24:36 +0200

    Creating RepositoryCell

---
+++ GitHub Repositories/Modules/Repositories/View/Repositories/RepositoryCell/RepositoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  RepositoryCell.swift
+//  GitHub Repositories
+//
+//  Created by Abdurrahman Alboghdady on 31/03/2023.
+//
+
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+
+    @IBOutlet weak var ownerImageView: UIImageView!
+    @IBOutlet weak var repositoryNameLabel: UILabel!
+    @IBOutlet weak var ownerNameLabel: UILabel!
+    @IBOutlet weak var creationDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
