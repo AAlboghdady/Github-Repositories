2e2535fe9e1f7c9005ec0c25cff830f767518413
Madar iOS <madar.developer.iosapps@gmail.com>
2023-03-31 00:26:28 +0200

    Creating RepositoriesVC design

---
+++ GitHub Repositories/Modules/Repositories/View/Repositories/RepositoriesVC/RepositoriesVC.swift
@@ -0,0 +1,20 @@
+//
+//  RepositoriesVC.swift
+//  GitHub Repositories
+//
+//  Created by Abdurrahman Alboghdady on 31/03/2023.
+//
+
+import UIKit
+
+class RepositoriesVC: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
