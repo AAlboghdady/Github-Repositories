dd12703c9b7acc154f796c21912d59be60a58779
Madar iOS <madar.developer.iosapps@gmail.com>
2023-03-31 00:48:34 +0200

    Creating Models

---
+++ GitHub Repositories/Modules/Repositories/Model/Repository/Repository.swift
@@ -0,0 +1,63 @@
+/* 
+Copyright (c) 2023 Swift Models Generated from JSON powered by http://www.json4swift.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar
+
+*/
+
+import Foundation
+
+struct Repository : Codable {
+	let id : Int?
+	let node_id : String?
+	let name : String?
+	let full_name : String?
+	let `private` : Bool?
+	let owner : Owner?
+	let html_url : String?
+	let description : String?
+	let fork : Bool?
+	let url : String?
+	let forks_url : String?
+	let keys_urls_url : String?
+	let collaborators_url : String?
+	let teams_url : String?
+	let hooks_url : String?
+	let issue_events_url : String?
+	let events_url : String?
+	let assignees_url : String?
+	let branches_url : String?
+	let tags_url : String?
+	let blobs_url : String?
+	let git_tags_url : String?
+	let git_refs_url : String?
+	let trees_url : String?
+	let statuses_url : String?
+	let languages_url : String?
+	let stargazers_url : String?
+	let contributors_url : String?
+	let subscribers_url : String?
+	let subscription_url : String?
+	let commits_url : String?
+	let git_commits_url : String?
+	let comments_url : String?
+	let issue_comment_url : String?
+	let contents_url : String?
+	let compare_url : String?
+	let merges_url : String?
+	let archive_url : String?
+	let downloads_url : String?
+	let issues_url : String?
+	let pulls_url : String?
+	let milestones_url : String?
+	let notifications_url : String?
+	let labels_url : String?
+	let releases_url : String?
+	let deployments_url : String?
+}
